2a6ced95da2121c94447073904ae3ca716d48f19
Forrest Collins <Forest.d.collins@gmail.com>
2017-06-03 01:51:59 -0500

    Updated the webpack configuration

--- web-app/webpack.config.js
+++ web-app/webpack.config.js
@@ -1,15 +1,73 @@
 const path = require('path');
+const webpack = require('webpack');
+
+const PATHS = {
+    client: path.join(__dirname, 'client'),
+    dist: path.join(__dirname, 'dist'),
+};
 
 module.exports = {
-  entry: './app/static/scripts/app.js',
-  output: {
-    filename: 'main.js',
-    path: path.join(__dirname, '/dist/static')
-  },
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        index: path.join(PATHS.app, 'index.jsx'),
+    },
 
-  module: {
-    rules: [
-      {test: /.js$/ , use: 'babel-loader'}
-    ]
-  }
-}
+    output: {
+        filename: '[name].js',
+        chunkFilename: '[name].chunk.js',
+        publicPath: '/',
+        path: path.join(PATHS.dist)
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)?$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',  
+            },
+
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            {
+                test: /\.svg?$/,
+                use: [
+                    {
+                        loader: 'svg-sprite-loader',
+                    },
+
+                    {
+                        loader: 'svgo-loader',
+                        options: {
+                            plugins: [
+                                { removeTitle: true },
+                                { convertColors: { shorthex: false }},
+                                { convertPathData: false},
+                            ],
+                        },
+                    },
+                ],
+
+                //include: path.resolve('./client/static/icons'),
+            },
+
+            {
+              test: /\.(jpg|png|webp)$/,
+              use: 'url-loader',
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.css', '.svg'],
+        alias: {
+         /* styles: path.resolve('./client/static/styles'),
+            icons: path.resolve('./client/static/icons'),
+            images: path.resolve('./client/static/images'),
+         */       
+        },
+    },
+};
